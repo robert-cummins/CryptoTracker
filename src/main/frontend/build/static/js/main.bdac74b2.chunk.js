8473b351c1e3578ad682a8c5ac10d5fec8be6d7c
robert-cummins <robert.k.cummins@gmail.com>
2021-12-07 17:18:57 +1300

    clean up React front end folder

---
+++ src/main/frontend/build/static/js/main.bdac74b2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(0));var d=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"HERE!!!!"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.bdac74b2.chunk.js.map
