b0adcbb00aeb2a84d07f700b9729e2a977acd1e0
robert-cummins <robert.k.cummins@gmail.com>
2021-12-14 11:54:41 +1300

    add tailwind dependancy

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <h1>HERE2!!!!</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
