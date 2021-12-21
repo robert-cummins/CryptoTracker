9bad100ff4e1bb64ad47f9bd5a7329c98a740e24
robert-cummins <robert.k.cummins@gmail.com>
2021-12-21 16:26:19 +1300

    transaction page more mobile responsive

--- src/main/frontend/src/components/Transactions.js
+++ src/main/frontend/src/components/Transactions.js
@@ -44,22 +44,22 @@ const Transactions = (props) => {
                 <DeleteModal deleteTransaction={deleteTransaction} hideDeleteWarning={hideDeleteWarning}/>
             }
 
-            <h2 className="text-4xl font-normal text-center leading-normal mt-0 mb-4 text-lime-500 font-VT233">TRANSACTIONS</h2>
-            <div className=" border-b border-lime-500  w-full sm:grid sm:grid-cols-5 gap-5">
-                <h4 className="font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Date</h4>
-                <h4 className="font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Crypto</h4>
-                <h4 className="font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Amount</h4>
-                <h4 className="font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Price</h4>
-                <h4 className=" font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Action</h4>
+            <h2 className="gap-2 border-b border-lime-500 text-4xl font-normal text-center leading-normal pb-4 mt-4 sm:mt-8 mb-4 text-lime-500 font-VT233">TRANSACTIONS</h2>
+            <div className="hidden border-b border-lime-500  w-full sm:grid sm:grid-cols-5 gap-5">
+                <h4 className="invisible sm:visible font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Date</h4>
+                <h4 className="invisible sm:visible font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Crypto</h4>
+                <h4 className="invisible sm:visible font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Amount</h4>
+                <h4 className="invisible sm:visible font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Price</h4>
+                <h4 className="invisible sm:visible font-normal sm:text-center leading-normal mt-0 mb-2 text-lime-500">Action</h4>
             </div>
             {transactions.map(transaction => {
                 return (
-                    <div className=" border-b border-lime-500  w-full sm:grid sm:grid-cols-5 gap-5">
-                        <h4 className="font-normal sm:text-center leading-normal mt-5 mb-4  text-white ">{transaction.created}</h4>
-                        <h4 className="font-normal sm:text-center leading-normal mt-5 mb-4 text-white">{transaction.crypto}</h4>
-                        <h4 className="font-normal sm:text-center leading-normal mt-5 mb-4 text-white">{transaction.amount}</h4>
-                        <h4 className="font-normal sm:text-center leading-normal mt-5 mb-4 text-white">{transaction.price}</h4>
-                        <button onClick={showDeleteWarning} value={transaction.id} class="w-17 h-7 bg-lime-600 hover:bg-lime-700 text-white font-bold px-4 rounded-full  sm:text-center m-auto">
+                    <div className="grid grid-cols-2 border-b border-lime-500  w-full sm:grid sm:grid-cols-5 sm:gap-5">
+                        <h4 className="font-normal text-center leading-normal mt-5 mb-4  text-white "><p className="visible sm:invisible text-lime-500">Date</p>{transaction.created}</h4>
+                        <h4 className="font-normal text-center leading-normal mt-5 mb-4 text-white"><p className="visible sm:invisible text-lime-500">Crypto</p>{transaction.crypto}</h4>
+                        <h4 className="font-normal text-center leading-normal mt-5 mb-4 text-white"><p className="visible sm:invisible text-lime-500">Amount</p>{transaction.amount}</h4>
+                        <h4 className="font-normal text-center leading-normal mt-5 mb-4 text-white"><p className="visible sm:invisible text-lime-500">Price</p>{transaction.price}</h4>
+                        <button onClick={showDeleteWarning} value={transaction.id} class="col-span-2 sm:col-span-1 w-17 h-7 bg-lime-600 hover:bg-lime-700 text-white font-bold px-4 rounded-full  sm:text-center m-auto mb-4">
                             Delete
                         </button>
                     </div>
